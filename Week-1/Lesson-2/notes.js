31c538548c9c7eefa48cc25814e673db2370356d
mosaka0 <m.onur7@hotmail.com>
2023-02-25 10:13:29 +0300

    change files path

---
+++ Week-1/Lesson-2/notes.js
@@ -0,0 +1,7 @@
+// git commit -m ""(m: Message)
+// git komutuyla bütün komutlar görünür.
+// git branch bulu
+// git checkout LOG_ID : Dallandırma yöntemi. Eski kodu getirerek inceleme yapmayı sağlayabilir.
+//
+
+console.log("hello");
