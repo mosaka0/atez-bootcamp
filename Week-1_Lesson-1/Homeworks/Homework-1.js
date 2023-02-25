e49b85c82334027d2ac31cc13c1ae93fbfc3e6b5
mosaka0 <m.onur7@hotmail.com>
2023-02-25 09:36:27 +0300

    fix code

--- Week-1_Lesson-1/Homeworks/Homework-1.js
+++ Week-1_Lesson-1/Homeworks/Homework-1.js
@@ -1,16 +1,22 @@
 //faktoriyel hesabı
 
-const findFactoriel = (a) => {
-    if(typeof a === "number"){
+const findFactoriel = (factorielNum) => {
+    if(typeof factorielNum === "number" && factorielNum > 0 && factorielNum !== Infinity){
         let result = 1;
-        for(let i = a ; i > 0 ; i--){
+        for(let i = factorielNum ; i > 0 ; i--){
             result *= i;
         }
         console.log(result);
+    }
+    else if(factorielNum === 0){
+        console.log(1);
+    }
+    else if(factorielNum < 0){
+        console.log("Lütfen pozitif bir sayı girin.");
     }
     else{
         console.log("Hata! Lütfen number tipinde değer gönderin");
     }
 }
 //Denemek için değer girin:
-findFactoriel(4);
+findFactoriel(5);
