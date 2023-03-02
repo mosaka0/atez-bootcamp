8e8a5dd7cfcc82a3552566931203c6a6f71c044e
mosaka0 <m.onur7@hotmail.com>
2023-03-02 17:02:26 +0300

    update lesson-2 research

--- Week-1/Lesson-2/research.ts
+++ Week-1/Lesson-2/research.ts
@@ -2,6 +2,7 @@
 
 //tslint :
 
+
 //Generics
 function displayUser <T , U>(name : T, id : U)  {
     return {name,id};
@@ -24,7 +25,72 @@ const getFullname: Fullname<Name ,Surname> = (name: Name, surname:Surname) => {
 }
 
 console.log(getFullname("onur","saka")); //mirac veya onur yazılabiliyor.
+
+//örnek
+const returnName = <T> (obj:T) => {
+    const id = Math.random();
+    return {
+        ...obj,
+        id,
+    };
+};
+
+interface Userinterface<T>{
+    name : string,
+    data : T,
+}
+
+const user: Userinterface<{meta: string}> = {
+    name : "Onur",
+    data : {
+        meta: "saka",
+    }
+}
+
+
+const result = returnName(user);
+console.log("result:",result);
+
+
+
 //Generics
 
 
-//
+
+//Enums
+const seasons = Object.freeze({
+    ilkbahar : "spring",
+    yaz: "summer",
+    sonbahar: "autumn",
+    kış: "winter"
+});
+//seasons.kış = "winter2"; -- yapılamaz çünkü object freeze yapıldı.
+console.log("kış:",seasons.kış);
+
+//Enums
+
+
+//Funtion Overloading
+
+function padding (a:number , b?:number , c?:number , d?: any){
+    if(b === undefined && c=== undefined && d=== undefined ){
+        b = c = d = a;
+    }
+    else if(c === undefined && d === undefined){
+        c = a;
+        d = b;
+    }
+    return{
+        top : a,
+        left : b,
+        bottom : c,
+        right : d,
+    };
+}
+//Funtion Overloading
+
+//:void nesneye bir değer atanamaz. undefined olur.
+
+
+
+
